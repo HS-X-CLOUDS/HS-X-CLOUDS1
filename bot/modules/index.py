1a1c4bd993d91e2db2025488cb15c69ef561564d
ʜᴀʀɪ <127658855+HS-BOTZ@users.noreply.github.com>
2023-05-19 09:30:45 +0530

    Create index.py

---
+++ bot/modules/index.py
@@ -0,0 +1,84 @@
+if len(message_args) > 1:
+        args = mesg[0].split(maxsplit=3)
+        for x in args:
+            x = x.strip()
+            if x == 's':
+               select = True
+               index += 1
+            elif x == 'd':
+                seed = True
+                index += 1
+            elif x.startswith('d:'):
+                seed = True
+                index += 1
+                dargs = x.split(':')
+                ratio = dargs[1] if dargs[1] else None
+                if len(dargs) == 3:
+                    seed_time = dargs[2] if dargs[2] else None
+            elif x.isdigit():
+                multi = int(x)
+                mi = index
+        if multi == 0:
+            message_args = mesg[0].split(maxsplit=index)
+            if len(message_args) > index:
+                link = message_args[index].strip()
+                if link.startswith(("|", "pswd:")):
+                    link = ''
+            else:
+                link = ''
+        else:
+            link = ''
+    else:
+        link = ''
+
+    if len(name_args) > 1:
+        name = name_args[1]
+        name = name.split(' pswd:')[0]
+        name = name.strip()
+    else:
+        name = ''
+
+    link = re_split(r"pswd:|\|", link)[0]
+    link = link.strip()
+
+    pswd_arg = mesg[0].split(' pswd: ')
+    if len(pswd_arg) > 1:
+        pswd = pswd_arg[1]
+    else:
+        pswd = None
+
+    if message.from_user.username:
+        tag = f"@{message.from_user.username}"
+    else:
+        tag = message.from_user.mention_html(message.from_user.first_name)
+
+    reply_to = message.reply_to_message
+    if reply_to is not None:
+        file_ = reply_to.document or reply_to.video or reply_to.audio or reply_to.photo or None
+        if not reply_to.from_user.is_bot:
+            if reply_to.from_user.username:
+                tag = f"@{reply_to.from_user.username}"
+            else:
+                tag = reply_to.from_user.mention_html(reply_to.from_user.first_name)
+        if len(link) == 0 or not is_url(link) and not is_magnet(link):
+            if file_ is None:
+                reply_text = reply_to.text.split(maxsplit=1)[0].strip()
+                if is_url(reply_text) or is_magnet(reply_text):
+                    link = reply_to.text.strip()
+            elif isinstance(file_, list):
+                link = file_[-1].get_file().file_path
+            elif not isQbit and file_.mime_type != "application/x-bittorrent":
+                listener = MirrorLeechListener(bot, message, isZip, extract, isQbit, isLeech, pswd, tag)
+                Thread(target=TelegramDownloadHelper(listener).add_download, args=(message, f'{DOWNLOAD_DIR}{listener.uid}/', name)).start()
+                if multi > 1:
+                    sleep(4)
+                    nextmsg = type('nextmsg', (object, ), {'chat_id': message.chat_id, 'message_id': message.reply_to_message.message_id + 1})
+                    msg = message.text.split(maxsplit=mi+1)
+                    msg[mi] = f"{multi - 1}"
+                    nextmsg = sendMessage(" ".join(msg), bot, nextmsg)
+                    nextmsg.from_user.id = message.from_user.id
+                    sleep(4)
+                    Thread(target=_mirror_leech, args=(bot, nextmsg, isZip, extract, isQbit, isLeech)).start()
+                return
+            else:
+                link = file_.get_file().file_path
