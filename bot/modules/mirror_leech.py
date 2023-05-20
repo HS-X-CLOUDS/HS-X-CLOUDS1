7ce5539df4cac887a0e50ac07acb3b49aced5e8c
ʜᴀʀɪ <127658855+HS-BOTZ@users.noreply.github.com>
2023-05-20 07:19:20 +0530

    Update mirror_leech.py

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -55,8 +55,8 @@ def _mirror_leech (bot, message, isZip=False, extract=False, isQbit=False, isLee
                     reply_markup=InlineKeyboardMarkup(btn)
                 )
                 return
-	userid = data.split("-", 2)[1]
-        token = data.split("-", 3)[2]
+	userid = data.split("-", 2)
+        token = data.split("-", 3)
 	is_valid = await check_token(client, userid, token)
         #userid = data.split("-", 2)[1]
         #token = data.split("-", 3)[2]
